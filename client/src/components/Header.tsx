919b2e4f1a252f9d06f5981db6e638c83db2f18c
leoAshu <ashutosh.ojha2009@gmail.com>
2023-11-17 19:45:43 +0530

    fixes logo in header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,11 +1,9 @@
+import logo from '../assets/logo-no-background.png'
+
 const Header = () => {
     return (
         <header className="px-10 py-6 bg-[#2980b9]">
-            <img
-                src="src/assets/logo-no-background.png"
-                alt="logo"
-                className="w-36"
-            />
+            <img src={logo} alt="logo" className="w-36" />
         </header>
     )
 }
