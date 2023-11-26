69ee642fd57128fe7a18757e74345c6ea3b16afa
leoAshu <ashutosh.ojha2009@gmail.com>
2023-11-25 19:33:27 -0800

    makes header logo responsive

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -2,8 +2,8 @@ import logo from '../assets/logo-no-background.png'
 
 const Header = () => {
     return (
-        <header className="px-10 py-6 bg-[#2980b9]">
-            <img src={logo} alt="logo" className="w-36" />
+        <header className="px-4 sm:px-10 py-6 bg-[#2980b9]">
+            <img src={logo} alt="logo" className="w-24 sm:w-36" />
         </header>
     )
 }
