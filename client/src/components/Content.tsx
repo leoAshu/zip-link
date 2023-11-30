952f0858e4f3f3b5d6da81912d080209029aaba7
leoAshu <ashutosh.ojha2009@gmail.com>
2023-11-29 17:44:01 -0800

    implements loader and loading state while backend booting up

--- client/src/components/Content.tsx
+++ client/src/components/Content.tsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import { Oval } from 'react-loader-spinner'
+
 import ZipLink from '../models'
 import linkIcon from '../assets/link.png'
 import { removeHttps } from '../utils'
@@ -7,6 +9,7 @@ import { removeHttps } from '../utils'
 const Content = () => {
     const [urlInput, setUrlInput] = useState('')
     const [zipLinks, setZipLinks] = useState([] as ZipLink[])
+    const [isLoading, setIsLoading] = useState(true)
 
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault()
@@ -65,6 +68,8 @@ const Content = () => {
             } catch (err) {
                 alert(err)
                 console.error(err)
+            } finally {
+                setIsLoading(false)
             }
         }
 
@@ -85,60 +90,90 @@ const Content = () => {
                     </p>
                 </div>
 
-                <div className="flex justify-center items-center bg-white rounded-lg p-2">
-                    <img src={linkIcon} alt="url" className="w-6 rounded-lg" />
-
-                    <input
-                        type="url"
-                        placeholder="Enter your URL"
-                        value={urlInput}
-                        onChange={(e) => setUrlInput(e.target.value)}
-                        required
-                        className="flex-1 outline-none rounded-md p-2 caret-transparent text-[#2980b9]"
-                    />
-
-                    <button
-                        type="submit"
-                        className="p-2 px-4 text-white text-lg font-semibold bg-[#3498db] hover:bg-[#2980b9] transition-all duration-300 rounded-md"
-                        onClick={handleSubmit}
-                    >
-                        Zip It
-                    </button>
-                </div>
-
-                {Boolean(zipLinks.length) && (
-                    <div className="hidden sm:block sm:w-full mt-4 shadow-md rounded-md overflow-clip transition-transform transform hover:scale-105">
-                        <table className="w-full bg-white">
-                            <thead className="bg-[#2980b9] text-white text-left">
-                                <tr>
-                                    <th className="p-4">Short Link</th>
-                                    <th className="p-4 pl-6">Original Link</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {zipLinks.map((item, index) => (
-                                    <tr
-                                        key={index}
-                                        className={`text-sm font-semibold text-[#2980b9] ${
-                                            index % 2 === 0
-                                                ? 'bg-white'
-                                                : 'bg-[#f2f2f2]'
-                                        } border-t border-t-[#f2f2f2] transition-all duration-300`}
-                                    >
-                                        <td className="p-4">
-                                            {`${removeHttps(
-                                                import.meta.env
-                                                    .VITE_APP_API_BASE_URL
-                                            )}/${item.zipId}`}
-                                        </td>
-                                        <td className="p-4 pl-6">
-                                            {removeHttps(item.redirectUrl)}
-                                        </td>
-                                    </tr>
-                                ))}
-                            </tbody>
-                        </table>
+                {isLoading ? (
+                    <div className="flex flex-col justify-center items-center">
+                        <Oval
+                            height={60}
+                            width={60}
+                            strokeWidth={5}
+                            strokeWidthSecondary={5}
+                            color="#FFF"
+                            secondaryColor="#FFF"
+                        />
+                        <p className="mt-4 font-semibold text-base md:text-xl text-center">
+                            Booting Up!
+                            <br />
+                            <span className="font-normal text-sm md:text-lg">
+                                Please be patient!
+                            </span>
+                        </p>
                     </div>
+                ) : (
+                    <>
+                        <div className="flex justify-center items-center bg-white rounded-lg p-2">
+                            <img
+                                src={linkIcon}
+                                alt="url"
+                                className="w-6 rounded-lg"
+                            />
+
+                            <input
+                                type="url"
+                                placeholder="Enter your URL"
+                                value={urlInput}
+                                onChange={(e) => setUrlInput(e.target.value)}
+                                required
+                                className="flex-1 outline-none rounded-md p-2 caret-transparent text-[#2980b9]"
+                            />
+
+                            <button
+                                type="submit"
+                                className="p-2 px-4 text-white text-lg font-semibold bg-[#3498db] hover:bg-[#2980b9] transition-all duration-300 rounded-md"
+                                onClick={handleSubmit}
+                            >
+                                Zip It
+                            </button>
+                        </div>
+
+                        {Boolean(zipLinks.length) && (
+                            <div className="hidden sm:block sm:w-full mt-4 shadow-md rounded-md overflow-clip transition-transform transform hover:scale-105">
+                                <table className="w-full bg-white">
+                                    <thead className="bg-[#2980b9] text-white text-left">
+                                        <tr>
+                                            <th className="p-4">Short Link</th>
+                                            <th className="p-4 pl-6">
+                                                Original Link
+                                            </th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {zipLinks.map((item, index) => (
+                                            <tr
+                                                key={index}
+                                                className={`text-sm font-semibold text-[#2980b9] ${
+                                                    index % 2 === 0
+                                                        ? 'bg-white'
+                                                        : 'bg-[#f2f2f2]'
+                                                } border-t border-t-[#f2f2f2] transition-all duration-300`}
+                                            >
+                                                <td className="p-4">
+                                                    {`${removeHttps(
+                                                        import.meta.env
+                                                            .VITE_APP_API_BASE_URL
+                                                    )}/${item.zipId}`}
+                                                </td>
+                                                <td className="p-4 pl-6">
+                                                    {removeHttps(
+                                                        item.redirectUrl
+                                                    )}
+                                                </td>
+                                            </tr>
+                                        ))}
+                                    </tbody>
+                                </table>
+                            </div>
+                        )}
+                    </>
                 )}
             </div>
         </main>
